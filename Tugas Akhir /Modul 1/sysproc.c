b82a62be5eb888c75f9881a86ed7c9b58f5b1c7b
Mhmmdfthn <mhmmdfthnt@gmail.com>
2025-07-18 14:36:33 +0700

    Create sysproc.c

---
+++ Tugas Akhir /Modul 1/sysproc.c
@@ -0,0 +1,125 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int readcount = 0; // untuk menghitung read()
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+nt
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int sys_getreadcount(void) {
+  return readcount;
+}
+
+#include "proc.h"
+#include "spinlock.h"
+
+extern struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+nt
+sys_getpinfo(void)
+{
+  struct pinfo *dest;
+
+  if (argptr(0, (char**)&dest, sizeof(*dest)) < 0)
+    return -1;
+
+  struct proc *p;
+  int i = 0;
+
+  acquire(&ptable.lock);  //    ^f^p ini ptable yang global (tabel proses kernel)
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+    if (p->state != UNUSED) {
+      dest->pid[i] = p->pid;
+      dest->mem[i] = p->sz;
+      safestrcpy(dest->name[i], p->name, sizeof(p->name));
+      i++;
+    }
+  }
+  release(&ptable.lock);
+  return 0;
+}
